40b9154e7d5f540d826907baf78316c0cdeaac9e
Mishrahariom <hariom.cs2020@mpgi.edu.in>
2023-10-28 00:54:35 +0530

    some changes on checkout

--- src/pages/shop/shop.jsx
+++ src/pages/shop/shop.jsx
@@ -8,6 +8,7 @@ export const Shop = () => {
     <div className="shop">
       <div className="shopTitle">
         <h1>PedroTech Shop</h1>
+        
       </div>
 
       <div className="products">
